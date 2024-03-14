088b69ad368f853ca567b1d0ecf00aa9b26faa52
sonseldeep <sonseldeep8890.shrestha@gmail.com>
2024-03-14 12:12:39 +0545

    Binary Search

---
+++ Searching/binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int num, int target)
+{
+
+  int low = 0;
+  int high = num - 1;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (arr[mid] == target)
+    {
+      return mid;
+    }
+    else if (target > arr[mid])
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  int num = 7;
+  int target = 1;
+  int result = binarySearch(arr, num, target);
+  cout << "result is at index  " << result << endl;
+
+  return 0;
+}
---
+++ Searching/binarySearchRecursive.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int BInarySearchRecursive(int arr[], int low, int high, int target)
+{
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (target == arr[mid])
+    {
+      return mid;
+    }
+
+    else if (target > arr[mid])
+    {
+      return BInarySearchRecursive(arr, mid + 1, high, target);
+    }
+
+    else
+    {
+      return BInarySearchRecursive(arr, low, mid - 1, target);
+    }
+  }
+  return -1;
+}
+
+int main()
+{
+
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int num = 9;
+  int target = 6;
+  int low = 0;
+  int high = num - 1;
+  int result = BInarySearchRecursive(arr, low, high, target);
+  cout << "The element is present at index: " << result;
+
+  return 0;
+}
